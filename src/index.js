7d644d5db198ddf8e7e36c36918bd00ade9a009e
kattrinkr <kattrinkr@mail.ru>
2018-03-02 08:53:36 +0300

    feature/seq(10)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  var i = 0, answer = 0, max = 0;
+  var temp = [];
+  for (i = 0; i < array.length-1; i++){
+    if (array[i+1]-array[i] === 1)
+    {  answer++;
+       temp.push(answer);
+    }
+    else if (answer !== 0){
+      answer = 0;
+    }
+  }
+  for (i = 0; i < temp.length; i++){
+    if (temp[i] > max){
+      max = temp[i];
+    }
+  }
+
+  return ++max;
 }
